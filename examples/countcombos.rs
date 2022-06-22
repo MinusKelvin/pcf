0534f5c9f39ab2d2b72effd15dec7df6473aa29a
MinusKelvin <mark.carlson@minuskelvin.net>
2022-06-22 15:34:54 +1000

    change countcombos to use local counts aggregated later

--- examples/countcombos.rs
+++ examples/countcombos.rs
@@ -7,8 +7,9 @@ fn main() {
     let count = AtomicU64::new(0);
     let t = std::time::Instant::now();
 
-    pcf::find_combinations_mt(pieces, pcf::BitBoard(0), &Default::default(), 4, |_| {
-        count.fetch_add(1, Ordering::Relaxed);
+    let mut incr = DelayedIncrement::new(&count);
+    pcf::find_combinations_mt(pieces, pcf::BitBoard(0), &Default::default(), 4, move |_| {
+        incr.inc();
     });
 
     println!(
@@ -16,4 +17,26 @@ fn main() {
         count.into_inner(),
         t.elapsed()
     );
+}
+
+#[derive(Clone)]
+struct DelayedIncrement<'a> {
+    local: u64,
+    target: &'a AtomicU64,
+}
+
+impl DelayedIncrement<'_> {
+    pub fn new(target: &AtomicU64) -> DelayedIncrement {
+        DelayedIncrement { local: 0, target }
+    }
+
+    pub fn inc(&mut self) {
+        self.local += 1;
+    }
+}
+
+impl Drop for DelayedIncrement<'_> {
+    fn drop(&mut self) {
+        self.target.fetch_add(self.local, Ordering::Relaxed);
+    }
 }
